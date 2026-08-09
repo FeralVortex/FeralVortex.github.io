189e65bf45c91de0883da6147916472dd910fab4
FeralVortex <nabil.sm397@gmail.com>
2026-08-09 18:26:14 +0600

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Supabase browser configuration.
 // Fill these two values after creating your free Supabase project.
 // The publishable/anon key is designed for browser use when RLS policies are enabled.
-window.PORTFOLIO_SUPABASE_URL = "PASTE_YOUR_SUPABASE_PROJECT_URL_HERE";
-window.PORTFOLIO_SUPABASE_KEY = "PASTE_YOUR_SUPABASE_PUBLISHABLE_OR_ANON_KEY_HERE";
+window.PORTFOLIO_SUPABASE_URL = "https://hozeeonmymsxxzwnsboj.supabase.co/rest/v1/";
+window.PORTFOLIO_SUPABASE_KEY = "sb_publishable_mI21vhB-TfMJBzDG4JqOGA_ZHNeggs4";
